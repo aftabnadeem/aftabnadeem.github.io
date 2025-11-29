259cb0df681fd67427802623da6e38b5227fe220
aftabnadeem <114148396+aftabnadeem@users.noreply.github.com>
2025-11-29 18:42:59 +0530

    font change

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Inter } from "next/font/google";
+
+export const inter = Inter({ 
+  subsets: ["latin"],
+  variable: "--font-inter", 
+});
 
 export const metadata = {
   title: "Aftab Nadeem | Full Stack Developer",
@@ -9,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#0D0D0D] text-white">
+      <body className={`${inter.variable} bg-[#0D0D0D] text-white`}>
         <Navbar />
         <div className="pt-20">{children}</div>
         <Footer />
